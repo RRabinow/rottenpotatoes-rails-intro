577aa8ad04964c4e063c08a0dd26889b125c9a0f
Ryan Rabinowitz <rrabinow@uccs.edu>
2018-02-24 01:33:15 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,17 +14,28 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     @all_ratings = ["G", "PG", "PG-13", "R"]
     
-    if(params[:sort_by] == "title")
+    if(params[:sort_by] == "title" or session[:sort_by].to_s == "title") #Check if title column was clicked or session has the last sort order
       @movies = @movies.order(:title)
+      session[:sort_by] = "title"
     end
-    if (params[:sort_by] == "release_date")
+    if (params[:sort_by] == "release_date" or session[:sort_by].to_s == "release_date")#Check if release_date column was clicked or session has the last sort order
       @movies = @movies.order(:release_date)
+      session[:sort_by] = "release_date"
     end
     @sort_column = params[:sort_by]
-    if (params[:ratings] != nil)
+    if(params[:ratings] != nil) # if boxes were checked, record them into session and selected_ratings
+      session[:rating] = params[:ratings] # the rating_pref check is so we don't 
       @selected_ratings = params[:ratings]
-      @movies = @movies.find_all{|m| @selected_ratings.include?(m.rating)}
+    else
+      if(session.has_key?(:rating))#if no boxes were checked but there is a session, use that in selected_ratings
+        @selected_ratings = session[:rating]
+      end
+    end#if no boxes were checked and there was no session, leave selected_ratings as nil and index view will auto check all boxes
+    
+    if (@selected_ratings != nil)
+      @movies = @movies.find_all{|m| @selected_ratings.include?(m.rating)}#filter by rating
     end
+      
   end
 
   def new
